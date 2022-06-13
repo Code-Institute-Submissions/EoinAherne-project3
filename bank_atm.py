2c880ca553f85a10e3c9ff95d737e10e7ce07a54
EoinAherne <aherneeoin88@gmail.com>
2022-06-13 19:05:26 +0000

    Add bank creation function and ability to create second user on account

--- bank_atm.py
+++ bank_atm.py
@@ -2,24 +2,22 @@
 Create Parent class for User
 """
 class user():
-    def __init__(self, name, age, gender):
+    def __init__(self, name, age,):
         self.name = name
         self.age = age
-        self.gender = gender
-
+        
     def user_details(self):
         print('User Details: ')
         print('Name: ', self.name)
         print('Age: ', self.age)
-        print('Gender: ', self.gender)
-
+        
 class bank(user):
     total_deposits = 0
     total_withdrawals = 0
 
 
-    def __init__(self, name, age, gender, balance):
-        super().__init__(name, age, gender)
+    def __init__(self, name, age, balance):
+        super().__init__(name, age)
         self.balance = balance
     
     def show_info(self):
@@ -41,3 +39,24 @@ class bank(user):
         self.balance -= wdraw
         self.total_withdrawals += 1 
 
+
+"""
+Create bank function which will populate the data of the user object
+"""
+def create_bank(name):
+    balance = float(input(f'{name.name.title()}, how much money do you have to open account with? '))
+    return balance
+
+while True:    
+    print('Welcome to EA bank')    
+    name = input('Enter your name')
+    age = int(input('Enter your age'))
+    user_one = user(name, age)
+    user_two = None
+    new_user = input('Would you like to add another user? Type No to continue creating your account. ')
+    if new_user.lower() == 'yes':
+        name = input('Enter the second users name: ')
+        age = input('Enter the second users age: ')
+        user_two = user(name, age)
+        print('Thank you for registering two users. Please create your accounts. ')
+        
