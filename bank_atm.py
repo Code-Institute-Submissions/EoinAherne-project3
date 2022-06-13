f9a21da659797b383f10cf6d37b6b72ea7bd16f7
EoinAherne <aherneeoin88@gmail.com>
2022-06-13 15:59:14 +0000

    Add comment template for program

---
+++ bank_atm.py
@@ -0,0 +1,10 @@
+#Create a Parent Class for the account holder
+#Stores user details
+#function which calls the information in the class, eg account holder
+
+#Create a child class for the bank
+#Holds info about account balances
+#functions which allow to withdraw, deposit, view balance
+
+
+
